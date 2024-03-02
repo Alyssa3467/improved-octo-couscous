e6b86365758bb845d32e01da3217c713d6be20e8
Alyssa Nguyen <18040427+Alyssa3467@users.noreply.github.com>
2024-03-01 22:15:01 -0800

    it works, I think…

--- toolchain-builder.sh
+++ toolchain-builder.sh
@@ -2,18 +2,18 @@
 # inspired by https://stackoverflow.com/questions/42022659/how-to-get-a-smaller-toolchain-from-scratch
 set -euo pipefail
 
-# export TARGET_BOARD=bcm27xx
-# export TARGET_SUBTARGET=bcm2708
-export TARGET_TRIPLET="arm-rpi-linux-gnueabihf"
-# export TARGET_ARCH="armv6kz+fp"
-# export CPU_TYPE="arm1176jzf-s+vfp"
 export TGT_FLAGS="--with-float=hard --with-fpu=vfp"
 export OWRT_TARGET="bcm27xx"
 export OWRT_SUBTARGET="bcm2708"
 export OWRT_PAIR="${OWRT_TARGET}/${OWRT_SUBTARGET}"
-export TGT_ARCH="armv6kz"
-export TGT_CPU="arm1176jzf-s"
-export TGT_CPU_EXT="vfp"
+# export ARCH="armv6kz+fp"
+export ARCH="arm"
+export CPU="arm1176jzf-s"
+# export CPU_EXT="vfp"
+# export CPU=arm
+export CLIB=musleabihf
+# export TARGET_TRIPLET=${CPU}-linux-${CLIB}
+export TARGET_TRIPLET=${ARCH}-linux-${CLIB}
 export CROSS_PREFIX="${PWD}/output/cross"
 export NATIVE_PREFIX="${PWD}/output/native"
 export SOURCE_DIR="${PWD}/sources"
@@ -22,9 +22,8 @@ export PATH=${CROSS_PREFIX}/bin:$PATH
 
 mkdir ${SOURCE_DIR} -p ; cd ${SOURCE_DIR}
 
-
 # doing this with spinning metal platters = bad idea 😅
-cd ${SOURCE_DIR}
+
 wget -N https://ftpmirror.gnu.org/binutils/binutils-2.42.tar.gz &
 wget -N https://ftpmirror.gnu.org/gcc/gcc-13.2.0/gcc-13.2.0.tar.gz &
 # wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.12.6.tar.bz2
@@ -33,17 +32,14 @@ wget -N https://ftpmirror.gnu.org/mpfr/mpfr-4.2.1.tar.gz &
 wget -N https://ftpmirror.gnu.org/gmp/gmp-6.3.0.tar.gz &
 wget -N https://ftpmirror.gnu.org/mpc/mpc-1.3.1.tar.gz &
 
-
-
-cd ${SOURCE_DIR}
 wait -f
 # **** Get Raspberry Pi headers ****
 (
-    cd ${SOURCE_DIR}
-   [[ -d raspberry-kernel ]] || git clone http://github.com/raspberrypi/linux raspberry-kernel
+   [[ -d raspberry-kernel ]] || git clone https://github.com/raspberrypi/linux raspberry-kernel
     cd raspberry-kernel
-    KERNEL=kernel make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_HDR_PATH="${CROSS_PREFIX}/${OWRT_PAIR}" bcmrpi_defconfig
-    KERNEL=kernel make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_HDR_PATH="${CROSS_PREFIX}/${OWRT_PAIR}" headers_install
+    git fetch
+    # KERNEL=kernel make ARCH=arm CROSS_COMPILE=${TARGET_TRIPLET}- INSTALL_HDR_PATH=${CROSS_PREFIX}/${TARGET_TRIPLET}  bcmrpi_defconfig
+    KERNEL=kernel make ARCH=${ARCH} CROSS_COMPILE=${TARGET_TRIPLET}- INSTALL_HDR_PATH=${CROSS_PREFIX}/${TARGET_TRIPLET}  headers_install
     echo -e "\n\nTarget headers installed\n\n"
     cd ..
 ) &
@@ -55,8 +51,7 @@ wait -f
 # **** GNU binutils (native) ****
 mkdir ${BUILD_DIR}/binutils -p ; cd ${BUILD_DIR}/binutils
 echo -e "\n\nMoved into $PWD\n\n"
-# ${SOURCE_DIR}/binutils-*/configure --prefix=${NATIVE_PREFIX} --disable-nls --disable-werror --disable-multilib
-${SOURCE_DIR}/binutils-*/configure --prefix=${NATIVE_PREFIX} --enable-werror=no --disable-multilib
+${SOURCE_DIR}/binutils-*/configure --prefix=${NATIVE_PREFIX} --disable-nls --enable-werror=no --disable-multilib
 
 # TODO: Make this a function?
 if (make -j$(nproc) -Orecurse); then
@@ -77,7 +72,7 @@ cd ${SOURCE_DIR}/gcc-*/
 [[ -h mpc  ]] || ln -s ../mpc-*/ mpc
 
 mkdir ${BUILD_DIR}/gcc -p ; cd ${BUILD_DIR}/gcc
-${SOURCE_DIR}/gcc-*/configure --prefix=${NATIVE_PREFIX}
+${SOURCE_DIR}/gcc-*/configure --prefix=${NATIVE_PREFIX} --disable-nls --enable-languages=c --disable-multilib
 echo -e "\n\nFinished configuring gcc\n\nBuilding gcc…"
 
 (
@@ -125,13 +120,10 @@ make install || make -d install || (echo "'make install' failed (gcc native)"; e
 # done with host system
 
 # **** GNU binutils (cross) ****
-mkdir ${BUILD_DIR}/${TGT_CPU}-binutils -p ; cd ${BUILD_DIR}/${TGT_CPU}-binutils
+mkdir ${BUILD_DIR}/${CPU}-binutils -p ; cd ${BUILD_DIR}/${CPU}-binutils
 echo "Moved into $PWD"
 
-${SOURCE_DIR}/binutils-*/configure --prefix=${CROSS_PREFIX} --target=${TARGET_TRIPLET} --with-sysroot=${CROSS_PREFIX}/${OWRT_PAIR} \
-  --enable-werror=no --disable-multilib --with-cpu=${TGT_CPU}
-#  --prefix=${CROSS_PREFIX} --with-sysroot \
-#  --with-cpu=${CPU_TYPE}  --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX} --disable-nls --disable-werror --disable-multilib
+${SOURCE_DIR}/binutils-*/configure --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX} --with-sysroot --disable-nls --disable-werror --disable-multilib
 if (make -j$(nproc) -Orecurse); then
   echo -e "\n\nmake binutils (cross) finished (first try!)\n\n"
 elif (read -p "'make binutils' (cross) problem with first attempt" ; make -j1 -d V=sc); then
@@ -145,24 +137,22 @@ make install
 # ******************************
 
 # **** GNU Compilier Collection (cross) ****
-mkdir ${BUILD_DIR}/bootstrap-${TGT_CPU}-gcc -p ; cd ${BUILD_DIR}/bootstrap-${TGT_CPU}-gcc
-${SOURCE_DIR}/gcc-*/configure --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX} --with-cpu=${TGT_CPU} ${TGT_FLAGS} \
-  --disable-threads --disable-multilib --disable-shared
+mkdir ${BUILD_DIR}/bootstrap-${CPU}-gcc -p ; cd ${BUILD_DIR}/bootstrap-${CPU}-gcc
+${SOURCE_DIR}/gcc-*/configure --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX} --disable-nls --enable-languages=c --disable-multilib --disable-threads --disable-shared --with-float=soft --with-cpu=${CPU}
 
-# ${SOURCE_DIR}/gcc-*/configure --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX} --disable-nls --enable-languages=c --disable-multilib
-# --disable-threads --disable-shared --with-float=hard  --with-arch=${TGT_ARCH}
 make all-gcc install-gcc
 make all-target-libgcc install-target-libgcc
 
 mkdir ${BUILD_DIR}/musl -p
 cd ${BUILD_DIR}/musl
-CC=${CROSS_PREFIX}/bin/${TARGET_TRIPLET}-gcc CFLAGS=-Wa,-mfloat-abi=hard ${SOURCE_DIR}/musl-*/configure  --prefix=${CROSS_PREFIX}/${TARGET_TRIPLET} --enable-optimize CROSS_COMPILE=${TARGET_TRIPLET}-
+# CC=${CROSS_PREFIX}/bin/${TARGET_TRIPLET}-gcc CFLAGS=-Wa,-mfloat-abi=hard ${SOURCE_DIR}/musl-*/configure  --prefix=${CROSS_PREFIX}/${TARGET_TRIPLET} --enable-optimize CROSS_COMPILE=${TARGET_TRIPLET}-
+CC=${TARGET_TRIPLET}-gcc CFLAGS=-Wa,-mfloat-abi=hard ${SOURCE_DIR}/musl-*/configure --prefix=${CROSS_PREFIX}/${TARGET_TRIPLET}/ --enable-optimize CROSS_COMPILE=${TARGET_TRIPLET}-
 make
 make install
 
-mkdir ${BUILD_DIR}/${TGT_CPU}-gcc -p
-cd ${BUILD_DIR}/${TGT_CPU}-gcc
-$SOURCE_DIR/gcc-*/configure --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX}  --with-cpu=${TGT_CPU} ${TGT_FLAGS} --disable-nls --enable-languages=c,c++ --disable-multilib --enable-threads --enable-shared --with-float=hard --enable-target-optspace --disable-libgomp --disable-libmudflap --without-isl --without-cloog --disable-decimal-float --disable-libssp --disable-libsanitizer --enable-lto --with-host-libstdcxx=-lstdc++
+mkdir ${BUILD_DIR}/${CPU}-gcc -p
+cd ${BUILD_DIR}/${CPU}-gcc
+${SOURCE_DIR}/gcc-*/configure --target=${TARGET_TRIPLET} --prefix=${CROSS_PREFIX} --disable-nls --enable-languages=c,c++ --disable-multilib --enable-threads --enable-shared --with-float=soft --with-cpu=${CPU} --enable-target-optspace --disable-libgomp --disable-libmudflap --without-isl --without-cloog --disable-decimal-float --disable-libssp --disable-libsanitizer --enable-lto --with-host-libstdcxx=-lstdc++
 
 make
 make install-strip
